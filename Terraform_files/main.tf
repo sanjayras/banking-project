c3eab9ac6503faa4a14d398b90676b1b30a972f1
root <root@ip-172-31-28-176.ec2.internal>
2025-10-11 11:17:03 +0000

    mine

---
+++ Terraform_files/main.tf
@@ -0,0 +1,24 @@
+resource "aws_instance" "test-server" {
+  ami = "ami-0360c520857e3138f"
+  instance_type = "t2.micro"
+  key_name = "bank"
+  vpc_security_group_ids = ["sg-00fc35b46c299fc5b"]
+  connection {
+     type = "ssh"
+     user = "ubuntu"
+     private_key = file("./bank.pem")
+     host = self.public_ip
+     }
+  provisioner "remote-exec" {
+     inline = ["echo 'wait to start the instance' "]
+  }
+  tags = {
+     Name = "test-server"
+     }
+  provisioner "local-exec" {
+     command = "echo ${aws_instance.test-server.public_ip} > inventory"
+     }
+  provisioner "local-exec" {
+     command = "ansible-playbook /var/lib/jenkins/workspace/financeme/Terraform_files/ansibleplaybook.yml""
+     }
+  }
